9ae6574378ef96b964d34d5554f200d5395a1093
rustam <kpytu3ha@yandex.ru>
2017-12-21 17:10:24 +0500

    Disable SA1200 rule

--- MongoRepo/Entity.cs
+++ MongoRepo/Entity.cs
@@ -1,8 +1,11 @@
-﻿namespace MongoRepo
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoRepo
 {
     public class Entity<TKey> : IEntity<TKey>
         where TKey : class
     {
+        [BsonId]
         public TKey Id { get; set; }
     }
 }
