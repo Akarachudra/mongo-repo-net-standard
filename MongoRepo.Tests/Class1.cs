000c4c718d9618812be8db2609ff193ba8d5c540
rustam <kpytu3ha@yandex.ru>
2017-12-27 14:33:24 +0500

    Change test project to .net core

---
+++ MongoRepo.Tests/Class1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace MongoRepo.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void SomeTest()
+        {
+        }
+    }
+}
